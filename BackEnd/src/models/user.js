aa7ac56e9f00980bebf15336e3b4f73da8b9d298
Yayehyrad <yayehyrad.sisay@gmail.com>
2024-01-08 12:31:58 +0300

    user role and stat

--- BackEnd/src/models/user.js
+++ BackEnd/src/models/user.js
@@ -1,96 +1,111 @@
-const mongoose = require("mongoose")
-const vaidator = require('validator') 
-const  bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-const Task = require("./task")
+const mongoose = require("mongoose");
+const vaidator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const Task = require("./task");
 
 const userSchima = new mongoose.Schema({
-    name : {
-        type:String,
-        required:true,
-        trim:true  
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  user_name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    Validate(value) {
+      if (!vaidator.isEmail(value)) {
+        throw new Error("nt email");
+      }
     },
-    email : {
-        type:String,
-        required:true ,
-        unique:true,
-        trim:true,
-        lowercase:true,
-        Validate(value){
-            if (!vaidator.isEmail(value)){
-                throw new Error("nt email")
-            }
-        }
+  },
+  age: {
+    type: Number,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlenght: 6,
+  },
+  role: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  status: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
     },
-    age : {
-        type:Number
-    },
-    password:{
-        type:String,
-        required:true,
-        trim:true,
-        minlenght:6
-    },
-    tokens:[{
-        token:{
-            type:String,
-            required:true
-        }
-    }]
-})
+  ],
+});
 
-userSchima.virtual('tasks' , {
-    ref:'Task',
-    localField:'_id',
-    foreignField:'owner'
-})
+userSchima.virtual("tasks", {
+  ref: "Task",
+  localField: "_id",
+  foreignField: "owner",
+});
 
-userSchima.methods.generateAuthToken = async function(){
-          const token = jwt.sign({_id:this._id.toString()},'fightlikeurthethirdmonkey' )
-          this.tokens = this.tokens.concat({token})
-          await this.save()
-          return token
-}
-userSchima.methods.toJSON =  function(){
-    const userObject = this.toObject()
-    
-    delete userObject.password
-    delete userObject.tokens
+userSchima.methods.generateAuthToken = async function () {
+  const token = jwt.sign(
+    { _id: this._id.toString() },
+    "fightlikeurthethirdmonkey"
+  );
+  this.tokens = this.tokens.concat({ token });
+  await this.save();
+  return token;
+};
+userSchima.methods.toJSON = function () {
+  const userObject = this.toObject();
 
-    return userObject
-}
-userSchima.statics.findByCredentias = async (email , password)=>{
-    
-            const user = await User.findOne({
-                email : email
-            })
-            if(!user){
-                throw new Error("Unable to login")
-            }
-            const isMatch = await bcrypt.compare(password , user.password)
-            if(!isMatch){
-                throw new Error(" invalid email or password")
-            }
-            return user
+  delete userObject.password;
+  delete userObject.tokens;
 
-}
+  return userObject;
+};
+userSchima.statics.findByCredentias = async (email, password) => {
+  const user = await User.findOne({
+    email: email,
+  });
+  if (!user) {
+    throw new Error("Unable to login");
+  }
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw new Error(" invalid email or password");
+  }
+  return user;
+};
 
+userSchima.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 8);
+  }
+  next();
+});
 
+userSchima.pre("remove", async function (next) {
+  await Task.deleteMany({ owner: this._id });
+  next();
+});
 
-userSchima.pre("save", async function(next){
-            if(this.isModified('password')){
-                this.password = await bcrypt.hash(this.password , 8)
-            }
-            next()
-})
+const User = mongoose.model("User", userSchima);
 
-userSchima.pre('remove' , async function(next){
-    await Task.deleteMany({owner:this._id})
-    next()
-})
-
-
-const User = mongoose.model('User' , userSchima
-)
-
-module.exports = User
+module.exports = User;
