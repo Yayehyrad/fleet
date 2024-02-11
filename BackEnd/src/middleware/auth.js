1fc484c2aca1b29ab1452bda1f864e6d3d2fdc24
Yayehyrad <yayehyrad.sisay@gmail.com>
2024-02-12 00:56:07 +0300

    upt

--- BackEnd/src/middleware/auth.js
+++ BackEnd/src/middleware/auth.js
@@ -3,9 +3,7 @@ const jwt = require("jsonwebtoken");
 const auth = async (req, res, next) => {
   try {
     const token = req.header("Authorization").replace("Bearer ", "");
-    console.log(token);
     const decoded = jwt.verify(token, "fightlikeurthethirdmonkey");
-    console.log(decoded);
     const user = await User.findOne({
       _id: decoded._id,
       "tokens.token": token,
